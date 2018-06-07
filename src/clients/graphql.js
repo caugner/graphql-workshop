6d28d27925fc6988ee96c5882c22930aa5ef62bf
Claas Augner <git@caugner.de>
2018-06-07 12:24:37 +0200

    Ex 4.4

--- src/clients/graphql.js
+++ src/clients/graphql.js
@@ -8,4 +8,24 @@ export async function getPosts(limit, offset) {
 
 export async function addNewPost(newPost) {}
 
-export async function addNewComment(comment, postId) {}
+export async function addNewComment(comment, postId) {
+  const result = await axios.post(
+    '/graphql',
+    {
+      query: `mutation createComment($postId: ID!, $content: String!) {
+        createComment (
+          postId: $postId,
+          content: $content
+        ) {
+          id
+          content
+        }
+      }`,
+      variables: {
+        postId,
+        content: comment.content
+      }
+    });
+  console.log(result);
+  return result.data.data.createComment;
+}
