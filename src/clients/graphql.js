380a78bfe407efbece19b0e8d28bb56a4cf7872b
Claas Augner <git@caugner.de>
2018-06-07 11:23:34 +0200

    Ex 2.1

--- src/clients/graphql.js
+++ src/clients/graphql.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export async function getPosts(limit, offset) {}
+export async function getPosts(limit, offset) {
+  const result = await axios.post('/graphql', { query: `{ posts { id, title, content } }` })
+  console.log(result);
+  return result.data.data.posts;
+}
 
 export async function addNewPost(newPost) {}
 
